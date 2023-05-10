eb234ccd5271b5040d096e616f2dd6c3c06ebdef
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-05-10 12:11:09 +0100

    config

--- fullstack-backend/src/app.ts
+++ fullstack-backend/src/app.ts
@@ -1,13 +1,19 @@
 import express, { Express } from 'express';
 import { DeliveryServer } from './setupServer';
 import databaseConnnection from './setupDatabase';
+import { config } from './config'
 
 class Application {
     public initialize(): void {
+        this.loadConfig();
         databaseConnnection();
         const app: Express = express();
         const server: DeliveryServer = new DeliveryServer(app);
         server.start();
+    }
+
+    private loadConfig(): void {
+        config.validateConfig();
     }
 }
 
