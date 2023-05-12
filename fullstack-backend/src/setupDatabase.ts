742c3cc0a92deb879cfaa404be317927edcddffe
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-05-12 21:01:46 +0100

    .ignore changes

--- fullstack-backend/src/setupDatabase.ts
+++ fullstack-backend/src/setupDatabase.ts
@@ -1,18 +1,22 @@
 import mongoose, { mongo } from 'mongoose';
+import Logger from 'bunyan';
 import { config } from './config';
 
-export default () => {
-    const connect = () => {
-        mongoose.connect(`${config.DATABASE_URL}`)
-            .then(() => {
-              console.log("Succesfully connected to database.");
-            })
-            .catch((error) => {
-              console.log("error connecting to database", error);
-              return process.exit(1);
-            })
-    };
-    connect();
+const log: Logger = config.createLogger('setupDatabase');
 
-    mongoose.connection.on('disconnected', connect);
+export default () => {
+	const connect = () => {
+		mongoose
+			.connect(`${config.DATABASE_URL}`)
+			.then(() => {
+				log.info('Succesfully connected to database.');
+			})
+			.catch((error) => {
+				log.error('error connecting to database', error);
+				return process.exit(1);
+			});
+	};
+	connect();
+
+	mongoose.connection.on('disconnected', connect);
 };
